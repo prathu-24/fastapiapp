570951cf9c7f5a67dde11f49488dfb4975904dd8
pratham <4al23ai042@aiet.org.in>
2026-06-29 12:10:31 +0530

    BACKEND DONE

--- models/company.py
+++ models/company.py
@@ -9,3 +9,4 @@ class Company(Base):
     email = Column(String,unique=True,)
     phone = Column(String, unique=True,)
     jobs = relationship("Job", back_populates="company")
+    location = Column(String)
