d12179359e73e09bc19f3ba01b806896270702ce
pratham <4al23ai042@aiet.org.in>
2026-06-29 09:39:37 +0530

    pg admin data

--- models/job.py
+++ models/job.py
@@ -1,19 +1,12 @@
-from sqlalchemy import Column, Integer, String, Enum,ForeignKey
-from models.company import Company
-from sqlalchemy.orm import declarative_base,relationship
-from database import Base , engine ,SessionLocal
-
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey
+from database import Base, engine, SessionLocal
+from sqlalchemy.orm import relationship
 
 class Job(Base):
     __tablename__ = "jobs"
-
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False, )
-    description = Column(String, nullable=False)
-    salary = Column(Integer, nullable=False)
-    company_id = Column(Integer, ForeignKey('companies.id'), nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    salary = Column(Integer)
+    company_id = Column(Integer, ForeignKey("companies.id"))
     company = relationship("Company", back_populates="jobs")
-
-
